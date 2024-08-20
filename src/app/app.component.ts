17ce7121c993b5dede4120644bdf055cc5d5f07b
Bruno Barcelos Dornellas <bruno.b.dornellas@hotmail.com>
2024-08-20 15:26:50 -0300

    template inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bank-front-angular';
+  sidebarOpen=true;
+
+  toogleSideBar(){
+    this.sidebarOpen=this.sidebarOpen ? false:true;
+  }
+  
 }
